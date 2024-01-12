9638a54937b432589c0eb601033d339c0eba4947
Dan <dan@dminc.ru>
2024-01-12 12:53:08 +0300

    fix: ts type errors

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useLayoutEffect, useRef, useState} from "react";
+import React, {useEffect, useRef, useState, MouseEvent} from "react";
 import {useResize} from "@/hooks/useResize";
 
 interface Fragment {
@@ -8,57 +8,40 @@ interface Fragment {
 
 export function Canvas() {
     const [isPressed, setPressed] = useState(false)
-    const canvasRef = useRef();
-    const [action, setAction] = useState([]);
-    const [stack, setStack] = useState([]);
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const [action, setAction] = useState<Fragment[]>([]);
+    const [stack, setStack] = useState<Fragment[][]>([]);
     const { width, height } = useResize();
     useEffect(() => {
-        // @ts-ignore
-        const ctx = canvasRef.current.getContext('2d');
-        ctx.width = window.innerWidth;
-        ctx.height = window.innerHeight;
+        const ctx = canvasRef.current!.getContext('2d');
         redrawCanvas(ctx);
     }, [width, height])
-    // @ts-ignore
-    function redrawCanvas(ctx) {
+
+    function redrawCanvas(ctx : CanvasRenderingContext2D | null) {
         for (let action of stack) {
             draw(ctx, action);
         }
     }
 
-    // @ts-ignore
-    function draw(ctx, action : Fragment[]) {
-        ctx.strokeStyle = 'rgba(0, 0, 0, 1)'
-        ctx.beginPath();
+    function draw(ctx : CanvasRenderingContext2D | null, action : Fragment[]) {
+        ctx!.strokeStyle = 'rgba(0, 0, 0, 1)'
+        ctx!.beginPath();
         for (let frag of action) {
-            ctx.lineTo(frag.x, frag.y);
+            ctx!.lineTo(frag.x, frag.y);
         }
-        ctx.stroke();
+        ctx!.stroke();
     }
 
-    // @ts-ignore
-    function handleMouseDown(event) {
+    function handleMouseDown(event : MouseEvent) {
         if (!isPressed) {
             console.log("down");
-            // @ts-ignore
-            const ctx = canvasRef.current.getContext('2d');
-            /*let redrawFlag = false;
-            if (ctx.width !== window.innerWidth || ctx.height !== window.innerHeight) {
-                redrawFlag = true;
-                console.log(ctx.width, window.innerWidth);
-                console.log(ctx.height, window.innerHeight);
-            }*/
-            ctx.strokeStyle = 'rgba(0, 0, 0, 1)'
+            const ctx = canvasRef.current!.getContext('2d');
+            ctx!.strokeStyle = 'rgba(0, 0, 0, 1)'
             let curAct = action;
-            /*if (redrawFlag) {
-                redrawCanvas(ctx);
-            }*/
-            ctx.beginPath();
-            // @ts-ignore
+            ctx!.beginPath();
             curAct.push({x: event.pageX, y: event.pageY});
-            ctx.lineTo(event.pageX, event.pageY);
-            ctx.stroke();
-            // @ts-ignore
+            ctx!.lineTo(event.pageX, event.pageY);
+            ctx!.stroke();
             setAction(curAct);
             setPressed(true);
         }
@@ -67,48 +50,34 @@ export function Canvas() {
     function handleMouseUp() {
         if (isPressed) {
             console.log("up");
-            // @ts-ignore
-            const ctx = canvasRef.current.getContext('2d');
-            ctx.stroke();
-            ctx.closePath();
-            // @ts-ignore
+            const ctx = canvasRef.current!.getContext('2d');
+            ctx!.stroke();
+            ctx!.closePath();
             stack.push(action);
-            /*if (stack.length > 10) {
-                // @ts-ignore
-                stack.shift();
-                console.log("hi")
-            }*/
             setAction([]);
             setStack(stack);
             setPressed(false);
-            // @ts-ignore
             console.log(stack);
         }
     }
 
-    // @ts-ignore
-    function handleMouseMove(event) {
+    function handleMouseMove(event : MouseEvent) {
         console.log("move");
         if (isPressed) {
-            // @ts-ignore
-            const ctx = canvasRef.current.getContext('2d');
+            const ctx = canvasRef.current!.getContext('2d');
             let curAct = action;
-            // @ts-ignore
             curAct.push({x: event.pageX, y: event.pageY});
-            ctx.lineTo(event.pageX, event.pageY);
-            ctx.stroke();
+            ctx!.lineTo(event.pageX, event.pageY);
+            ctx!.stroke();
             setAction(curAct);
         }
     }
 
-    // @ts-ignore
     function handleLeave() {
         if (isPressed) {
-            // @ts-ignore
-            const ctx = canvasRef.current.getContext('2d');
-            ctx.stroke();
-            ctx.closePath();
-            // @ts-ignore
+            const ctx = canvasRef.current!.getContext('2d');
+            ctx!.stroke();
+            ctx!.closePath();
             stack.push(action);
             setAction([]);
             setStack(stack);
@@ -119,7 +88,6 @@ export function Canvas() {
     return <canvas
         width={width}
         height={height}
-        // @ts-ignore
         ref={canvasRef}
         onMouseDown={handleMouseDown}
         onMouseUp={handleMouseUp}
