614b486c1ccb6aef29632755232d8df983e6fec2
Dan <dan@dminc.ru>
2024-01-18 14:56:57 +0300

    feat: send fragments by WS and fix out of range error

--- src/store/reducers/CanvasSlice.tsx
+++ src/store/reducers/CanvasSlice.tsx
@@ -1,31 +1,40 @@
 import {Fragment} from "@/app/_hooks/useStack";
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {useAppDispatch} from "@/app/_hooks/redux";
+import {sendFragment} from "@/store/reducers/SocketSlice";
 
 interface CanvasState {
     roomId: string;
     stack: Fragment[][];
 }
 
+interface ActionData {
+    actionId: number,
+    fragment: Fragment
+}
+
 const initialState: CanvasState = {
     roomId: "",
-    stack: [[]]
+    stack: []
 }
 
 export const canvasSlice = createSlice({
     name: 'canvas',
     initialState,
     reducers: {
-        joinFragment(state: CanvasState, action: PayloadAction<Fragment>) {
-            state.stack[state.stack.length-1].push({
-                x: action.payload.x,
-                y: action.payload.y
-            });
+        joinFragment(state: CanvasState, action: PayloadAction<ActionData>) {
+            // if canvas will go out of sync, then there is possible out of range exception!
+            if (state.stack.length == action.payload.actionId) state.stack.push([]);
+            state.stack[action.payload.actionId].push(action.payload.fragment);
+            const dispatch = useAppDispatch();
+            dispatch(sendFragment(action.payload))
         },
-        endAction(state: CanvasState) {
-            state.stack.push([]);
+        joinRoom(state: CanvasState, action: PayloadAction<CanvasState>) {
+            state.roomId = action.payload.roomId;
+            state.stack = action.payload.stack;
         }
     }
 })
 
-export const { joinFragment, endAction } = canvasSlice.actions;
+export const { joinFragment, joinRoom } = canvasSlice.actions;
 export default canvasSlice.reducer;
