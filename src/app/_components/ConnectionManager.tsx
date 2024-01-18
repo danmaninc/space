2592da8825ea7cf0ce628ae27173094e318478f4
Dan <dan@dminc.ru>
2024-01-18 13:13:42 +0300

    feat: add nickname field

--- src/app/_components/ConnectionManager.tsx
+++ src/app/_components/ConnectionManager.tsx
@@ -1,9 +1,19 @@
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, MouseEventHandler, useState} from "react";
 
 export function ConnectionManager() {
+    const [nickname, setNickname] = useState("");
     const [room, setRoom] = useState("");
 
-    function changeInputValue(e: ChangeEvent<HTMLInputElement>) {
+
+    function tryJoining() {
+
+    }
+    function changeNicknameValue(e: ChangeEvent<HTMLInputElement>) {
+        if (e.target.value.length <= 16) {
+            setNickname(e.target.value.trim().replace(RegExp("[^A-Z]", "i"), ""))
+        }
+    }
+    function changeRoomValue(e: ChangeEvent<HTMLInputElement>) {
         if (e.target.value.length <= 6) {
             setRoom(e.target.value.toUpperCase().trim().replace(RegExp("[^A-Z]"), ""))
         }
@@ -13,10 +23,21 @@ export function ConnectionManager() {
         <h3 className="text-center text-md">join your friends!</h3>
         <input
             className="bg-gray-300 text-center text-gray-700 font-medium rounded-sm w-36"
-            onChange={changeInputValue}
+            onChange={changeNicknameValue}
+            placeholder="your nickname"
+            value={nickname}
+        />
+        <input
+            className="bg-gray-300 text-center text-gray-700 font-medium rounded-sm w-36"
+            onChange={changeRoomValue}
             placeholder="enter room code..."
             value={room}
         />
-        <button className="bg-gray-400 rounded-md px-1">join</button>
+        <button
+            className="bg-gray-400 rounded-md px-1"
+            onClick={tryJoining}
+        >
+            join
+        </button>
     </div>
 }
